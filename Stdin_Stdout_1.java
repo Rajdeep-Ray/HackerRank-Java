82f69b595a9c3f2f2313028c23def6cdf4693bbf
Rajdeep Ray <rayrajdeep2001@gmail.com>
2020-06-01 19:43:45 +0530

    Renamed

--- Stdin_Stdout_1.java
+++ Stdin_Stdout_1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Stdin_Stdout {
+public class Stdin_Stdout_1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
